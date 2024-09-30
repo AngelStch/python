fb0e66453463393160966a8461683e0bc4018363
Angel Stoychev <adstoichev@gmail.com>
2024-09-30 16:25:47 +0300

    Some Basics
    I'm solving exercises form Softuni.

---
+++ firstSteps/intro1.py
@@ -0,0 +1,41 @@
+import math
+# 1 exc
+
+# for i in range(101):
+#     print(i)
+
+
+# 2 exc
+# n = int(input())
+# for i in range(1,n+1,3):
+#   print(i)
+
+# exc 3
+# n = int(input())
+# for i in range(0,n, 2):
+#   print(f"{math.pow(2,i):.0f}")
+
+# exc 7
+# n = int(input())
+# sum = 0
+# for i in range(n):
+#     sum+=int(input())
+
+# print(sum)
+
+# exc 10
+# n = int(input())
+# sumL = 0
+# sumR = 0
+# for i in range(n):
+#     if(i%2==0):
+#         sumL+=int(input())
+#     else:
+#        sumR+=int(input())
+
+# if(sumL == sumR):
+#     print("Yes")
+#     print(f"Sum = {sumR}")
+# else:
+#     print("No")
+#     print(f"Diff = {abs(sumL-sumR)}")
