fbc088dad2b28e9f5733cc85e63dcc1beaa70553
Angel Stoychev <122397062+AngelStch@users.noreply.github.com>
2024-10-07 15:46:57 +0300

    lerning the basics of the language

---
+++ laboratorno1.py
@@ -0,0 +1,12 @@
+# import  math
+# r =int(input())
+# print(f'obikolka: {(math.pi+r)*2:0.3f}')
+# print(f'lice: {(math.pi+math.pow(r,2)):0.3f}')
+
+# chasove = int(input("Въведерте часовете: "))
+# tarifa = int(input("Въведерте тарифа за час: "))
+# print(f"Брутно заплащане: {chasove*tarifa}")
+
+celsius = int(input("Въведете t* в целси: "))
+print(f"Темперарурата в фаренхайт ще е {celsius*9/5 +32}")
+
---
+++ lecture3.py
@@ -0,0 +1,23 @@
+import random
+
+
+# dir1,dir2,dir3 = map(int,input().split(","))
+# print(dir1,dir2,dir3)
+
+
+# a = map(int,input().split(","))
+# print(list(a))
+
+
+# data = []
+# for i in range(4):
+#     data.append(i)
+# print(end="\n")
+# print(data)
+
+
+data = []
+for i in range(10):
+    data.append(random.randint(-10,10))
+
+print(data)
