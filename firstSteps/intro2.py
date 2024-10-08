a6b73f436812d9f872f916a42c5ee75dab84334e
Angel Stoychev <adstoichev@gmail.com>
2024-10-08 19:08:31 +0300

    testing errors and nested loops

---
+++ firstSteps/intro2.py
@@ -0,0 +1,38 @@
+# Write a Python program to check the sum of three elements 
+# (each from an array) from three arrays is equal to a target value.
+#  Print all those three-element combinations.
+
+# x = map(int, input("x = ").split())
+# x = list(x)
+# y= map(int, input("y = ").split())
+# y = list(y)
+# z = map(int, input("z = ").split())
+# z = list(z)
+# dedicatedSum = int(input())
+# isfount = False
+# for i in range(len(x)):
+#    for c in range(len(y)):
+#       for j in range(len(z)):
+#          if(x[i]+y[c]+z[j]==dedicatedSum):
+#             print(f"purvoto sreshtane na zadsdenata suma e pri elementite {i+1} = {x[i]} ot masiv x, {c+1} = {y[c]} ot masiv y, {j+1} = {z[j]} ot masiv z")
+#             isfount = True
+#             break
+# if(isfount == False):
+#    print("Ne moje da se napravi takava suma ot 3te masiva")
+# print("x = ", x)
+# print("y = ", y)
+# print("z = ", z)
+
+   
+
+# Write a Python program that accepts six numbers as input and sorts them in descending order.
+# Input consists of six numbers n1, n2, n3, n4, n5, n6 (-100000 <= n1, n2, n3, n4, n5, n6 <= 100000). The six numbers are separated by a space.
+try:
+    nums = map(int, input("Enter the nums you want to sort (6):\n").split())
+    nums = list(nums)
+    if(len(nums)>6):
+        raise ValueError()
+    nums.sort()
+    print(nums)
+except ValueError:
+    print("All numbers must be between -100000 and 100000 and max count is 6")
--- firstSteps/myReserches.py
+++ firstSteps/myReserches.py
@@ -4,3 +4,5 @@ lst = [n*n for n in range(1,11)]
 lst = map( lambda x : str(x), lst)
 lst2 = ', '.join(lst)
 print(lst2)
+
+
