9c491f75e1ac3babf239bf0ffe131330db7c168c
Angel Stoychev <adstoichev@gmail.com>
2024-10-25 23:48:15 +0300

    solving

--- lecture4.py
+++ lecture4.py
@@ -19,6 +19,30 @@ import random
 # print(queue)
 # print(queue.popleft())
 
+def test(combined):
+    ls = []
+    s2 = ""
+    for s in combined.replace(' ', ''):
+        s2 += s
+        if s2.count("(") == s2.count(")"):
+            ls.append(s2)
+            s2 = ""
+    return ls
+
+combined = '( ()) ((()()())) (()) ()'
+print("Parentheses string:")
+print(combined)
+print("Separate parentheses groups of the said string:")
+print(test(combined))
+
+combined = '() (( ( )() (  )) ) ( ())'
+print("\nParentheses string:")
+print(combined)
+print("Separate parentheses groups of the said string:")
+print(test(combined))
+
+
+
 data = [ random.randint(-20,20) for i in range(20)]
 suma = map(sum,data)
 print(suma)
